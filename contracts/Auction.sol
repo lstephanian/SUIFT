1b3aa08e67eb7636f6b341c49219d9845a015eca
lstephanian <stephanianlm@gmail.com>
2023-09-09 08:20:47 -0700

    compiles

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -12,6 +12,7 @@ contract Auction is ERC1155Holder {
     //here, we will create a list of one address for examples sake and the contract will only know this address attended the event
     mapping(address  => bool) private attendeeOracle;
 
+    address public immutable TICKETS_ADDRESS = 0x55eBc20b2c938cfccfFEe1707B7604fB3C8703E0; //right now this is me
     uint public immutable AUCTION_START_TIME;
     uint public immutable AUCTION_END_TIME;
     uint public immutable REBATE_END_TIME;
@@ -57,7 +58,8 @@ contract Auction is ERC1155Holder {
         TICKET_RESERVE_PRICE = _ticketReservePrice;
 
         //mint the specific 1155 tickets
-        Tickets.mint(_auctionTicketsId, _ticketSupply);
+        Tickets tickets = Tickets(TICKETS_ADDRESS);
+        tickets.mint(_auctionTicketsId, _ticketSupply);
 
         //adding msg.sender to the event attendee list
         attendeeOracle[msg.sender] = true;
@@ -72,16 +74,16 @@ contract Auction is ERC1155Holder {
 
         uint minBidIndex = _minBidIndex();
 
-        require(msg.value > bids[minBidIndex], 'Your bid lower than the minimum');
+        require(msg.value > minBidIndex, 'Your bid lower than the minimum');
         
         //create a bid struct: beneficiary, bid amount, time of bid
-        Bid memory bid = Bid(msg.sender, msg.value, block.timestamp); 
+        Bid memory bid = Bid(payable(msg.sender), msg.value, block.timestamp); 
 
         //check whether number of bids is less than total ticket supply
         if (TICKET_SUPPLY > bids.length) {
             
             //add bid to bid struct and bidder to winner map
-            bids.push(Bid);
+            bids.push(bid);
             winners[msg.sender] = msg.value;
 
             //emit event indicating a bid has been placed
@@ -105,7 +107,7 @@ contract Auction is ERC1155Holder {
         require(success);
 
         return(success);
-        emit BitRefundReceived(msg.sender, amount);
+        // emit BitRefundReceived(msg.sender, amount);
     }
 
 
@@ -116,7 +118,7 @@ contract Auction is ERC1155Holder {
         require(!ended, "auctionEnd has already been called.");
 
         ended = true;
-        emit AuctionEnded();
+        // emit AuctionEnded();
 
         //TODO: Send 1155 NFTs to winners
     }
@@ -127,13 +129,13 @@ contract Auction is ERC1155Holder {
 
     //replaces one of the lowest accepted bids in the auction with this latest bid and adds a refund to the beneficiary who was overbid
     //Note: this could be improved by looping through timestamps and determining who was the most recent lowest bid
-    function _replaceLowestBid(Bid storage bid, uint256 minBidIndex) internal {
-        Bid storage currentBid = bids[minBidIndex];
+    function _replaceLowestBid(Bid memory bid, uint256 minBidIndex) internal {
+        Bid memory currentBid = bids[minBidIndex];
         bidRefunds[currentBid.beneficiary] += currentBid.amount;
         currentBid = bid;
         
         //add address of this bid to the winners list
-        winners[currentBid.winner] = currentBid.winningBid;
+        winners[currentBid.beneficiary] = currentBid.amount;
 
     }
 
@@ -141,7 +143,7 @@ contract Auction is ERC1155Holder {
     function _minBidIndex() internal view returns (uint256 minIndex) {
         uint minAmount;
         for(uint256 i; i < bids.length; i++) {
-            Bid storage bid = bids[i];
+            Bid memory bid = bids[i];
 
             if (bid.amount < minAmount || minAmount == 0) {
                     minIndex = i;
@@ -153,7 +155,7 @@ contract Auction is ERC1155Holder {
     function _wonAuction(address participant) private returns(bool){
         require(_isAuctionActive() == false, "Auction still ongoing");
         require(block.timestamp >= AUCTION_START_TIME, "The auction is not yet active");   
-        if(winners[participant]){
+        if(winners[participant] != 0){
             return(true);
         }
     }
@@ -189,13 +191,13 @@ contract Auction is ERC1155Holder {
         require(success);
 
         return(success);
-        emit BitRefundReceived(msg.sender, delta);        
+        // emit BitRefundReceived(msg.sender, delta);        
     }
     
     function burnRebate(address participant) private{
         require(msg.sender == owner, "You need to be the owner of the contract to do this");
         require(_isRebatePeriod(), "It's not rebate period");
-        require(_attendedConcert(), "You did not attend the event");
+        require(_attendedConcert(participant), "You did not attend the event");
 
         uint burnAmt = winners[participant];
         address burnAddy = 0x000000000000000000000000000000000000dEaD;
--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.8.8;
 import { Auction } from './Auction.sol';
 
 contract AuctionFactory {
-    address[] public auctions;
+    // address[] public auctions;
+    Auction[] private auctions;
     uint public immutable AUCTION_START_TIME = block.timestamp;
     uint public immutable AUCTION_LENGTH = 432000; //30 days
     uint public immutable REBATE_LENGTH = 432000; //60 days
@@ -13,42 +14,41 @@ contract AuctionFactory {
     uint public immutable middleRowTicketSupply = 250;
     uint public immutable frontRowTicketSupply = 250;
     uint public immutable floorSeatTicketSupply = 250;
-    uint public immutable noseBleedTicketPrice = 0.5;
-    uint public immutable middleRowTicketPrice = 0.75;
-    uint public immutable frontRowTicketPrice = 1;
-    uint public immutable floorSeatTicketPrice = 2;
+    uint public immutable noseBleedTicketPrice = 1;
+    uint public immutable middleRowTicketPrice = 2;
+    uint public immutable frontRowTicketPrice = 3;
+    uint public immutable floorSeatTicketPrice = 4;
     uint public immutable noseBleedId = 3; 
     uint public immutable middleRowId = 2;
     uint public immutable frontRowId = 1;
     uint public immutable floorSeatId = 0;
 
-
-    event AuctionCreated(address auctionContract, address owner, uint numAuctions, address[] allAuctions);
+    event AuctionCreated(Auction auctionContract, address owner, uint numAuctions);
 
     //create our 4 auctions
-    function createNosebleedAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) {
+    function createNosebleedAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) public {
         Auction newAuction = new Auction(noseBleedId, AUCTION_START_TIME, AUCTION_LENGTH, REBATE_LENGTH, noseBleedTicketSupply, noseBleedTicketPrice);
         auctions.push(newAuction);
 
-        AuctionCreated(newAuction, msg.sender, auctions.length, auctions);
+        emit AuctionCreated(newAuction, msg.sender, auctions.length);
     }
-    function createMiddleRowAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) {
+    function createMiddleRowAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) public {
         Auction newAuction = new Auction(middleRowId, AUCTION_START_TIME, AUCTION_LENGTH, REBATE_LENGTH, middleRowTicketSupply, middleRowTicketPrice);
         auctions.push(newAuction);
 
-        AuctionCreated(newAuction, msg.sender, auctions.length, auctions);
+        emit AuctionCreated(newAuction, msg.sender, auctions.length);
     }
-    function createFrontRowAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) {
+    function createFrontRowAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) public {
         Auction newAuction = new Auction(frontRowId, AUCTION_START_TIME, AUCTION_LENGTH, REBATE_LENGTH, frontRowTicketSupply, frontRowTicketPrice);
         auctions.push(newAuction);
 
-        AuctionCreated(newAuction, msg.sender, auctions.length, auctions);
+        emit AuctionCreated(newAuction, msg.sender, auctions.length);
     }
-    function createFloorSeatAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) {
+    function createFloorSeatAuction(uint _auctionTicketsId, uint startTime, uint biddingLength, uint rebateLength, uint ticketSupply, uint ticketReservePrice) public {
         Auction newAuction = new Auction(floorSeatId, AUCTION_START_TIME, AUCTION_LENGTH, REBATE_LENGTH, floorSeatTicketSupply, floorSeatTicketPrice);
         auctions.push(newAuction);
 
-        AuctionCreated(newAuction, msg.sender, auctions.length, auctions);
+        emit AuctionCreated(newAuction, msg.sender, auctions.length);
     }
 
     // function allAuctions() returns (address[]) {
